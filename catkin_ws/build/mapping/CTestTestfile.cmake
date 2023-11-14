1cdebe3d5afc69777b98e151460eb77d33170194
Alex Bethel <abethel@cs.nmt.edu>
2023-11-14 16:58:27 -0700

    Remove files that should be ignored

--- catkin_ws/build/mapping/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ndev/Documents/nmt/map_test/catkin_ws/src/mapping
-# Build directory: /home/ndev/Documents/nmt/map_test/catkin_ws/build/mapping
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
